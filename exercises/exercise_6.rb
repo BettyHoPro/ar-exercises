46a522e0adb0bf1b11bdfcb75c2e8b53a3fafafb
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-31 10:01:58 -0700

    feat: add employees under store 1 and store 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Betty", last_name: "Ho", hourly_rate: 80)
+@store1.employees.create(first_name: "Jason", last_name: "Acton", hourly_rate: 50)
+@store1.employees.create(first_name: "Jimmy", last_name: "Zheng", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Nate", last_name: "Dave", hourly_rate: 80)
+@store2.employees.create(first_name: "Morgan", last_name: "Lily", hourly_rate: 55)
+@store2.employees.create(first_name: "Nola", last_name: "Love", hourly_rate: 60)
+@store2.employees.create(first_name: "Miko", last_name: "Nika", hourly_rate: 65)
+
