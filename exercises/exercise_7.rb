82e01224fdac5b54e226236ff2c8ce15a5a85564
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-31 11:23:18 -0700

    feat:setting uup validations for store and employees models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# new_name = gets.chomp
+# Store.create!(
+#   name: new_name,
+# )
+
+
+puts "Fill out the store name"
+new_store_name = gets.chomp
+
+@new_store = Store.create(
+  name:new_store_name
+)
+
+puts @new_store.errors.full_messages
+
+
